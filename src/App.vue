1a762d074d982fd2644a7c9a1ade7c66ed02b8e4
mrcsmchll <manchinelli.m@gmail.com>
2026-02-25 20:20:09 -0300

    agrego componente usando o ` vgc -s`

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,18 @@
   <Conversor msg="Conversor de Temperatura"/>
   <br>
   <hr>
-  <p>Respuestas: 1:C - 2:B - 3:C - 4:A</p>
+  <Respuestas />
 </template>
 
 <script>
 import Conversor from './components/Conversor.vue'
+import Respuestas from './components/Respuestas.vue';
 
 export default {
   name: 'App',
   components: {
-    Conversor
+    Conversor,
+    Respuestas
   }
 }
 </script>
---
+++ src/components/Respuestas.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+
+  <section class="src-components-respuestas">
+      <p>Respuestas: 1:C - 2:B - 3:C - 4:A</p>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'src-components-respuestas',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .src-components-respuestas {
+
+  }
+</style>
